f46a001d9bd8edd2ac8c9a9547d9ea00c5b0fe56
jake <palatable7@naver.com>
2022-09-17 11:34:02 +0900

    Initial Commit

---
+++ ExMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExMVVM
+//
+//  Created by 김종권 on 2022/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExMVVM/ExMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExMVVMApp.swift
+//  ExMVVM
+//
+//  Created by 김종권 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct ExMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
